2eb4a9404f71a96535861fe4d37b39df65eed160
Peter Ayad <swordoflight8847@yahoo.com>
2021-01-26 16:43:09 -0800

    update project 6

--- app.js
+++ app.js
@@ -1,84 +1,96 @@
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
-const btn_reset = document.getElementByClassName('btn_reset');
-const missed = document.createElement('missed');
-const isLetter = document.createElement('isLetter');
+const btn_reset = document.getElementsByClassName('btn__reset')[0];
+// const missed = document.createElement('missed');
+// const isLetter = document.createElement('isLetter');
 // const isLetter = true;
-const space = document.createElement('space');
+// const space = document.createElement('space');
 let missed = 0;
-const phrases = document.createElement('phrases');
+// const phrases = document.createElement('phrases');
 
+const overlay = document.getElementById('overlay');
 btn_reset.addEventListener('click', (e) => {
-    if (e.target.className === 'btn_reset') {
-    btn_reset.style.display = 'none';
-} 
+    // if (e.target.className === 'btn_reset') {
+    // btn_reset
+    overlay.style.display = 'none';
+});
 
-phrases = [
+ const phrases = [
     'Set up baseCamp or a buffet',
     'Genetically engineered Tyrannosaur',
     'The Islands of La Cinco Muertes',
     'That was not on Ingens list',
     'The destruction of Isla Nublar',
-]
+];
 
 //function that gets a random phrase from array
 function getRandomPhraseAsArray(arr) {
-    phrases = arr[Math.floor(Math.random() * arr.length)];
-        for (let i = 0; i < phrases.length; i += 1) {
-            phrases[i].style.display = '';
+    return arr[Math.floor(Math.random() * arr.length)].split('');
+// for (let i = 0; i < phrases.length; i += 1) {
+        //     phrases[i].style.display = '';
     // for (let i = 0; i < arr.length; i += 1) {
     //     arr[i].arr.style.display = ''; 
     // }
-        getRandomPhraseAsArray(phrases);
+        // getRandomPhraseAsArray(phrases);
         // phrases = arr[Math.floor(Math.random() * arr.length)];
         // for (let i = 0; i < phrases.length; i += 1) {
         //     phrases[i].arr.style.display = '';
 
+        // }
+        // return phrases[i];
         }
-        return phrases[i];
-}
+getRandomPhraseAsArray(phrases);
+
+
+
 
 //function that adds letters of phrase to display
 function addPhraseToDisplay(arr) {
    for (let i = 0; i < arr.length; i += 1) {
-    let li = document.createElement('li');
-    li.appendChild('ul#phrase');
+       const list = arr.array[i];
+    list = document.createElement('li');
+    document.getElementsByClassName('letter').appendChild('ul#phrase > li');
+   
+    // li.appendChild('ul#phrase');
 
-    if (isLetter) {
-        phrases[i].addItemInput.value;
-    } else {
-        display = 'space';
-    }
+    // if (isLetter) {
+    //     phrases[i].addItemInput.value;
+    // } else {
+    //     display = 'space';
+    // }
    }
 
 }
 
 const phraseArray = getRandomPhraseAsArray(phrases);
-addPhrasetoDisplay(phraseArray);
+addPhraseToDisplay(phraseArray);
 
 //function that checks for letter in phrase
 const checkLetter = button => {
-const li = document.getElementsByTagName(li);
-const match = document.createElement('match');
+const li = document.getElementsByTagName('li');
+// const match = document.createElement('match');
 let match = null;
 
-for (let i = 0; i < li.length; i += 1) {
-if (match) {
-    li.style.display = 'show';
-    li.button.textContent = 'match';
-}
-}
-return match;
-}
+// for (let i = 0; i < li.length; i += 1) {
+// if (match) {
+//     li.style.display = 'show';
+//     li.button.textContent = 'match';
+// }
+// }
+// return match;
+};
 
 qwerty.addEventListener('click', (e) => {
-    let click = button.filter(function(btn_reset) {
-        return click;
+    // let click = button.filter(function(btn_reset) {
+    //     return click;
     });
-    checkLetter(match.value);
-} 
+    // checkLetter(match.value);
+
 
 //function that checks if game has been won or lost
 const checkWin = () => {
+
+};
+
 
 }
